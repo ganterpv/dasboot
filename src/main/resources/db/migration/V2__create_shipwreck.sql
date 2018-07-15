cf1a6d00383bf4aa73cfb74b65e7c4fcdc2abed6
Peter Ganter <Peter_Ganter@dialog.com.au>
2018-07-15 19:38:17 +1000

    Nearly finished
    Has a bug listing shipwrecks

---
+++ src/main/resources/db/migration/V2__create_shipwreck.sql
@@ -0,0 +1,10 @@
+create table SHIPWRECK (
+	ID int auto_increment,
+	NAME varchar(255),
+	DESCRIPTION varchar(2000),
+	CONDITION varchar(255),
+	DEPTH int,
+	LATITUDE double,
+	LONGITUDE double,
+	YEAR_DISCOVERED int
+);
